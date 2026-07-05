1a5522f66007a4ced686d4ec17bdc4a463acdde5
g4urav1 <gauravkumarjha2107@gmail.com>
2026-07-05 10:57:31 +0530

    reset page fixed

--- src/pages/resetPassword.jsx
+++ src/pages/resetPassword.jsx
@@ -3,6 +3,7 @@ import icon from "../assets/icon.svg"
 import meta from "../assets/meta.svg"
 import famora from "../assets/famora.svg"
 import { FaFacebook } from "react-icons/fa";
+import { X } from "lucide-react";
 import { useNavigate } from "react-router-dom"
 import { useContext, useEffect, useState } from "react"
 import { MobileContext } from "../context/context"
@@ -10,10 +11,14 @@ import { MobileContext } from "../context/context"
 export default function ResetPass() {
     const now = new Date();
     const year = now.getFullYear();
-   
     const navigate = useNavigate();
 
     const { isMobile, setIsMobile } = useContext(MobileContext)
+    const [dropdown, setDropdown] = useState(false)
+
+    function handleSubmit() {
+        setDropdown(true)
+    }
 
     useEffect(() => {
         const checkScreenSize = () => {
@@ -52,8 +57,8 @@ export default function ResetPass() {
                 <div className="p-4 space-y-3">
                     <h1 className="text-2xl font-semibold">Find your account</h1>
                     <p>
-                       Enter your username or email.{" "}
-                       <a href="#">Can't reset your password?</a>
+                        Enter your username or email.{" "}
+                        <a href="#">Can't reset your password?</a>
                     </p>
                     <div className="relative mb-5 w-full">
                         <input
@@ -64,7 +69,6 @@ export default function ResetPass() {
                             autoFocus
                             className="peer w-full rounded-[22px] border border-[#4b5563] bg-[#1d1d20] px-6 pb-3 pt-8 text-white outline-none transition-all duration-200 focus:border-accent focus:ring-4 focus:ring-cyan-400/10 mb-3"
                         />
-
                         <label
                             htmlFor="account"
                             className="pointer-events-none absolute left-6 top-2 text-[14px] font-medium text-[#b8c4d4] transition-all duration-200 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:translate-y-0 peer-focus:text-[14px]"
@@ -74,73 +78,63 @@ export default function ResetPass() {
                     </div>
                     <button
                         type="submit"
+                        onClick={() => { handleSubmit() }}
                         className="w-full rounded-full bg-primary py-3 text-base font-semibold text-white  transition hover:scale-[1.01] active:scale-[0.99]"
                     >
                         Next
                     </button>
-
                 </div>
-              
             </main>}
 
-            {false && <main className="bg-bg relative min-h-[calc(100vh-70px)] border-b border-[#494D53]/60 flex flex-col md:w-1/2 md:mx-auto justify-center items-center">
+            {dropdown && <main onClick={() => { setDropdown(false) }} className="absolute inset-0 bg-bg/10 backdrop-blur-[2px] flex items-center justify-center">
+
+                <div  onClick={(e) => e.stopPropagation()} className="w-full md:w-2/5 bg-surface rounded-lg p-4 space-y-3">
+                    <div className="flex justify-between"><div className="text-xl font-semibold">Check your Email or phone</div><X onClick={() => { setDropdown(false) }} /></div>
+                    <p>If an account matches the information you entered, you'll receive
+                        a message with a link to help you log back in.</p>
+                    <button
+                        type="submit"
+                        className="w-full rounded-full bg-primary py-3 text-base font-semibold text-white  transition hover:scale-[1.01] active:scale-[0.99]"
+                    >
+                        OK
+                    </button>
+                </div>
+            </main>}
+
+            {false && <main className="bg-bg relative min-h-[calc(100vh-70px)] border-b border-[#494D53]/60 flex flex-col md:w-1/2 md:mx-auto  items-center">
                 <div className="p-4 space-y-3">
-                    <h1 className="text-2xl font-semibold">What's your birthday?</h1>
+                    <h1 className="text-2xl font-semibold">Reset yor password</h1>
                     <p >
-                        Use your own birthday, even if this account is for a
-                        business, a pet or something else. No one will see this
-                        unless you choose to share it. Why do I need to
-                        provide my birthday?
+                        Create a password with at least 6 letters and numbers. You'll need this password to
+                        log into your account.
                     </p>
                     <div className="relative mb-5 w-full">
                         <input
-                            type="date"
-                            id="date"
-                            max={today}
-                            value={birthday}
+                            type="password"
+                            id="newPass"
                             autoFocus
-                            onChange={(e) => {
-                                const selectedDate = e.target.value;
-
-                                setBirthday(selectedDate);
-
-                                if (selectedDate > today) {
-                                    setError("Birthday cannot be in the future.");
-                                } else {
-                                    setError("");
-                                }
-                            }}
+                            placeholder=" "
                             className="peer w-full rounded-[22px] border border-[#4b5563] bg-[#1d1d20] px-6 pb-3 pt-8 text-white outline-none transition-all duration-200 focus:border-accent focus:ring-4 focus:ring-cyan-400/10 mb-3"
                         />
-
                         <label
-                            htmlFor="date"
+                            htmlFor="newPass"
                             className="pointer-events-none absolute left-6 top-2 text-[14px] font-medium text-[#b8c4d4] transition-all duration-200 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:translate-y-0 peer-focus:text-[14px]"
                         >
-                            Birthday
+                            New Password
                         </label>
-                        {error && (
-                            <p className="text-sm text-red-400">
-                                {error}
-                            </p>
-                        )}
                     </div>
                     <button
                         type="submit"
                         className="w-full rounded-full bg-primary py-3 text-base font-semibold text-white  transition hover:scale-[1.01] active:scale-[0.99]"
                     >
-                        Next
+                        Continue
                     </button>
-
                 </div>
-                <footer className="mt-auto text-center mb-4">
-                    <button className="text-secondary hover:bg-white/10 px-2 py-1">I already have an account</button>
-                </footer>
             </main>}
 
-            
 
-           
+
+
 
 
             {!isMobile && <footer className="px-4 py-7">
