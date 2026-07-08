86e2f63b51bd9bb4cd92e8ff105b48ce67b2a90a
g4urav1 <gauravkumarjha2107@gmail.com>
2026-07-08 21:32:23 +0530

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
-import { useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { LoginContext, MobileContext } from "../context/context";
 import famora from "../assets/icon.svg"
-import { House, PlaySquare } from "lucide-react";
+
+import { HomeIcon, HomeIconFill } from "../assets/home.jsx";
+import { ReelIcon, ReelIconFill } from "../assets/reel.jsx";
+import { Message, MessageFill } from "../assets/message.jsx";
+import { Search, SearchFill } from "../assets/search.jsx";
+import { Plus } from "../assets/create.jsx";
+import { Heart } from "../assets/notification.jsx";
 
 
 export default function Home() {
   const { loggedin } = useContext(LoginContext);
   const navigate = useNavigate();
 
+  const [active, setActive] = useState("home");
 
 
   // useEffect(() => {
@@ -27,20 +34,27 @@ export default function Home() {
 
         <nav>
           <ul>
-            <li>
-              <a href="#" className="flex">
-             <House/>  <span>Home</span>
+            <li onClick={() => setActive("home")}>
+              <a href="#"
+                className="flex items-center gap-2"
+              >
+                {active == "home" ? <HomeIconFill /> : <HomeIcon />}
+                <span>Home</span>
               </a>
             </li>
 
-            <li>
-              <a href="#" className="flex">
-               <PlaySquare/> <span>Reels</span>
+            <li onClick={() => setActive("reel")}>
+              <a href="#"
+
+                className="flex items-center gap-2"
+              >
+                {active == "reel" ? <ReelIconFill /> : <ReelIcon />}
+                <span>Reels</span>
               </a>
             </li>
-
-            <li>
-              <a href="#" className="flex">
+            <li onClick={() => setActive("message")}>
+              <a href="#" className="flex items-center gap-2">
+                {active == "message" ? <MessageFill /> : <Message />}
                 <span>Messages</span>
               </a>
             </li>
