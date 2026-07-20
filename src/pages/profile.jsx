549825ab294e5eb3526cc0b3aedc7831afe1e57a
g4urav1 <gauravkumarjha2107@gmail.com>
2026-07-20 10:13:00 +0530

    created demo profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,13 +1,75 @@
 import Sidebar from "../components/Sidebar";
+import pfp from "../assets/Profile.jpg";
+import { postcss } from "autoprefixer";
+import { useContext } from "react";
+import { MobileContext } from "../context/context";
+import Nav from "../components/nav"
 export default function ProfilePage() {
+  const posts = [
+    "https://picsum.photos/300?1",
+    "https://picsum.photos/300?2",
+    "https://picsum.photos/300?3",
+    "https://picsum.photos/300?4",
+    "https://picsum.photos/300?5",
+    "https://picsum.photos/300?6",
+    "https://picsum.photos/300?7",
+    "https://picsum.photos/300?8",
+    "https://picsum.photos/300?9",
+    "https://picsum.photos/300?10",
+    "https://picsum.photos/300?11",
+    "https://picsum.photos/300?12",
+    "https://picsum.photos/300?13",
+    "https://picsum.photos/300?14",
+    "https://picsum.photos/300?15"
+  ]
+
+  const {isMobile, setIsMobile} = useContext(MobileContext)
   return (
-    <div className="bg-bg text-text">
+    <div className="bg-bg text-text h-screen">
+      {isMobile && <Nav/>}
       <Sidebar />
 
-      <main className="pl-16 p-4">
-        profile
+      <main className="md:pl-16 md:p-4 mx-auto md:w-4/5 p-2 flex flex-col items-center">
+        <div className="flex gap-8 items-center md:w-3/5">
+          <img
+            src={pfp}
+            alt="Profile Picture"
+            className="w-28 h-28 rounded-full"
+          />
+          <div>
+            <div>
+              <h2 className="text-xl font-bold">g4urav1_</h2>
+              <p className="text-muted">Gaurav</p>
+            </div>
+            <div className="flex gap-2 ">
+              <p className="text-text">
+                {posts.length} <span className="text-text">Posts</span>
+              </p>
+              <p className="text-text">
+                18 <span className="text-text">Followers</span>
+              </p>
+              <p className="text-text">
+                18 <span className="text-text">Following</span>
+              </p>
+            </div>
+            <div className="text-sm">Caperidium</div>
+          </div>
+        </div>
+        <div className="flex gap-2 w-full md:w-3/5 mx-auto mt-10 ">
+          <button className="bg-gray-500/50 w-1/2 py-2 rounded-lg">Edit Profile</button><button className="bg-gray-500/50 w-1/2 py-2 rounded-lg">Create Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 mt-3 mb-[52px]">
+          {posts.map((post, i) => (
+            <div key={i}>
+              <img src={post} alt="" />
+            </div>
+          ))}
+
+        </div>
+
       </main>
     </div>
   );
 }
+
 
